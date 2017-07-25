8b128566fed4c7088c0ac0d4bacd223c727d94c0
Mario Zamora <maza1387@gmail.com>
2017-07-25 11:04:39 -0700

    Add Route Parameters

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json')
+const { cards } = data;
 
 router.get('/', (req, res) => {
-  res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardId}?side=question`)
+})
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+
+  const templateData = { id, text };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+  }
+
+  res.render('card', templateData);
 });
 
 module.exports = router;
