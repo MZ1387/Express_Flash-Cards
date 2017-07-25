1922812fdf84d282e19f7fa2fd21f37166e04012
Mario Zamora <maza1387@gmail.com>
2017-07-25 12:08:40 -0700

    Create Static Server, Create Public folder and Static Assets

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
